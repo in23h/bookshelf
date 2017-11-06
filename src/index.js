82a42968b84682e6c89475d9b99a54621c62e5cf
in23h <in23h@yahoo.com>
2017-11-05 17:40:15 -0800

    Updated basename on BrowserRouter

--- src/index.js
+++ src/index.js
@@ -5,6 +5,6 @@ import App from './App'
 import './index.css'
 
 ReactDOM.render(
-  <BrowserRouter basename={`${process.env.PUBLIC_URL}`} ><App /></BrowserRouter>,
+  <BrowserRouter basename="/bookshelf" ><App /></BrowserRouter>,
   document.getElementById('root')
 )
